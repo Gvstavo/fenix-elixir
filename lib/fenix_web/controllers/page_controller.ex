9c5050b021ec1b3f223a706ab7139fede4391ba2
arch <gustavopaivads@gmail.com>
2026-07-06 22:52:49 -0300

    first commit

---
+++ lib/fenix_web/controllers/page_controller.ex
@@ -0,0 +1,7 @@
+defmodule FenixWeb.PageController do
+  use FenixWeb, :controller
+
+  def home(conn, _params) do
+    render(conn, :home)
+  end
+end
---
+++ lib/fenix_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule FenixWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use FenixWeb, :html
+
+  embed_templates "page_html/*"
+end
